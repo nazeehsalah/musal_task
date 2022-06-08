9db72b276ed5ecf20b1163e61ac1576ea007fb7e
nazeeh salah <nazeehsalah28@gmail.com>
2022-06-08 19:49:24 +0200

    swip build files

--- back_deploy.sh
+++ back_deploy.sh
@@ -1,9 +1,8 @@
-echo '--------------Front-end Start--------------'
-cd front-end
-echo '--------------Installing Packages--------------'
+echo '--------------Backend Start--------------'
+cd back-end
+echo 'Installing Packages ..'
 npm install
-echo '--------------Start Test--------------'
-ng test --watch=false
-echo '--------------Start Build production--------------'
-ng build --prod
-echo 'front end project dist on ./front-end/dist/front-end '
+echo '--------------Start Back-end Test--------------'
+npm run test
+echo '--------------npm run dev--------------'
+npm run dev
--- front_deploy.sh
+++ front_deploy.sh
@@ -1,8 +1,9 @@
-echo '--------------Backend Start--------------'
-cd back-end
-echo 'Installing Packages ..'
+echo '--------------Front-end Start--------------'
+cd front-end
+echo '--------------Installing Packages--------------'
 npm install
-echo '--------------Start Back-end Test--------------'
-npm run test
-echo '--------------npm run dev--------------'
-npm run dev
+echo '--------------Start Test--------------'
+ng test --watch=false
+echo '--------------Start Build production--------------'
+ng build --prod
+echo 'front end project dist on ./front-end/dist/front-end '
